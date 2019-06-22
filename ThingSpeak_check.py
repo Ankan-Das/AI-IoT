c0935eef024e848c97a782d541bc7ea32fbe7665
Ankan-Das <46451509+Ankan-Das@users.noreply.github.com>
2019-06-22 15:58:07 +0530

    Add files via upload

---
+++ ThingSpeak_check.py
@@ -0,0 +1,13 @@
+import urllib.request
+import time
+import math
+from bs4 import BeautifulSoup
+#data = ('Ankan',20,'IITK')
+add = 50
+val=100
+while val<1200:
+    add+=50
+    data = urllib.request.urlopen("https://api.thingspeak.com/update?api_key=Q0P8D9GLKCHSMR7B&field1="+str(val))
+    print(data)
+    val += add + 50
+    time.sleep(5)
